431a9151918dbbc78694de7a22c1ed72732ecc00
Sohel Rana <expartdesigns5@gmail.com>
2023-08-07 13:30:24 +0600

    createUserWithEmailAndPassword

---
+++ src/firebaseConfig.jsx
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC-9Px1bqFqc2us_-uEzKktxtm_Iy21GFA",
+  authDomain: "chatt-1a90f.firebaseapp.com",
+  projectId: "chatt-1a90f",
+  storageBucket: "chatt-1a90f.appspot.com",
+  messagingSenderId: "1028934437053",
+  appId: "1:1028934437053:web:b57690f1205be95e180fd4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default firebaseConfig
--- src/pages/Registration.jsx
+++ src/pages/Registration.jsx
@@ -1,12 +1,94 @@
-import React from "react";
+import React, { useState } from "react";
+import {
+    getAuth,
+    createUserWithEmailAndPassword,
+    updateProfile,
+    sendEmailVerification,
+} from "firebase/auth";
+
+import { getDatabase, ref, set } from "firebase/database";
 // material css
-import { Grid, TextField, Button } from "@mui/material";
+import { Grid, TextField, Button, Alert, Stack } from "@mui/material";
+import LoadingButton from "@mui/lab/LoadingButton";
 // images
 import images from "../assets/chatt..png";
 // react icon
-import {AiFillEyeInvisible, AiFillEye} from "react-icons/ai"
+import { AiFillEyeInvisible, AiFillEye } from "react-icons/ai";
+import { useNavigate } from "react-router-dom";
+
+// inishalvalue
+let inishalvalue = {
+    email: "",
+    fullname: "",
+    password: "",
+    eye: false,
+    error: "",
+    loadding: false,
+};
 
 const Registration = () => {
+    const auth = getAuth();
+    const db = getDatabase();
+    let navigate = useNavigate();
+    let [value, settvalue] = useState(inishalvalue);
+
+    let handleChange = (e) => {
+        settvalue({
+            ...value,
+            [e.target.name]: e.target.value,
+        });
+    };
+    //handleSingup button
+    let handleSingup = () => {
+        let { email, fullname, password } = value;
+        if (!email) {
+            settvalue({
+                ...value,
+                error: "please your email address ",
+            });
+            return;
+        }
+        if (!fullname) {
+            settvalue({
+                ...value,
+                error: "please your full name  ",
+            });
+            return;
+        }
+        if (!password) {
+            settvalue({
+                ...value,
+                error: "please your password  ",
+            });
+            return;
+        }
+        settvalue({
+            ...value,
+            loadding: true,
+        });
+        // createUserWithEmailAndPassword chatt app
+        createUserWithEmailAndPassword(auth, email, password).then((user) => {
+            console.log(user);
+            updateProfile(auth.currentUser, {
+                displayName: fullname,
+            }).then(() => {
+                sendEmailVerification(auth.currentUser).then(() => {
+                    set(ref(db, "users/" + user.user.uid), {
+                        username: fullname,
+                        email: email,
+                    });
+                });
+            });
+            settvalue({
+                email: "",
+                fullname: "",
+                password: "",
+                loadding: false,
+            });
+            navigate("/login")
+        });
+        //
+    };
     return (
         <Grid
             container
@@ -17,33 +99,84 @@ const Registration = () => {
             xs={12}
         >
             <div className="Registration">
-                <img src={images} alt="logo" />
-                <h2 className="">Get started with easily register</h2>
-                <h4>Free register and you can enjoy it</h4>
+                <div className="regHadding">
+                    <img src={images} alt="logo" />
+                    <h2 className="">Get started with easily register</h2>
+                    <h4>Free register and you can enjoy it</h4>
+                </div>
                 <div className="RegInput">
                     <TextField
                         id="outlined-basic"
                         label="Email"
                         variant="outlined"
+                        name="email"
+                        type="email"
+                        value={value.email}
+                        onChange={handleChange}
                     />
+                    {value.error.includes("email") && (
+                        <Stack
+                            sx={{ width: "100%", margin: "10px 0" }}
+                            spacing={2}
+                        >
+                            <Alert severity="error">{value.error}</Alert>
+                        </Stack>
+                    )}
                 </div>
                 <div className="RegInput">
                     <TextField
                         id="outlined-basic"
                         label="Full Name"
                         variant="outlined"
+                        name="fullname"
+                        onChange={handleChange}
                     />
+                    {value.error.includes("full") && (
+                        <Stack
+                            sx={{ width: "100%", margin: "10px 0" }}
+                            spacing={2}
+                        >
+                            <Alert severity="error">{value.error}</Alert>
+                        </Stack>
+                    )}
                 </div>
                 <div className="RegInput">
                     <TextField
                         id="outlined-basic"
                         label="Password"
                         variant="outlined"
+                        name="password"
+                        onChange={handleChange}
+                        type={value.eye ? "text" : "password"}
                     />
-                    <AiFillEyeInvisible />
-                    <AiFillEye />
+                    <div
+                        onClick={() => settvalue({ ...value, eye: !value.eye })}
+                        className="eye-reg"
+                    >
+                        {value.eye ? <AiFillEye /> : <AiFillEyeInvisible />}
+                    </div>
+                    {value.error.includes("password") && (
+                        <Stack
+                            sx={{ width: "100%", margin: "10px 0" }}
+                            spacing={2}
+                        >
+                            <Alert severity="error">{value.error}</Alert>
+                        </Stack>
+                    )}
                 </div>
-                <Button className="button" variant="contained">Sign up</Button>
+                {value.loadding ? (
+                    <LoadingButton loading variant="outlined">
+                        Submit
+                    </LoadingButton>
+                ) : (
+                    <Button
+                        onClick={handleSingup}
+                        className="button"
+                        variant="contained"
+                    >
+                        Sign up
+                    </Button>
+                )}
             </div>
         </Grid>
     );
